ced9ba4fffd79e1d819439e18302baceac788d85
chan <96chan@berkeley.edu>
2014-04-25 14:36:57 -0700

    add infowindow accordingly with station position

--- _js/script.js
+++ _js/script.js
@@ -1,10 +1,14 @@
 
 var map ='';
+var gmarkers = [];    
 var sf_latlng = new google.maps.LatLng(37.78992,-122.3822776); // San Francisco
 var sj_latlng = new google.maps.LatLng(37.339508, -121.872193);       // San Jose
 var rc_latlng = new google.maps.LatLng(37.485217, -122.212308);       // Redwood City
 var mv_latlng = new google.maps.LatLng(37.395499, -122.078598);       // Mountain View
 var pa_latlng = new google.maps.LatLng(37.436707, -122.131716);       // Palo Alto
+var infowindow = new google.maps.InfoWindow({
+        maxWidth: 300,      //Doesn't work
+});
 
 $(document).ready(function() {
   initialize();
@@ -50,6 +54,31 @@ $('#control').click(function() {
 //---------------------------------
 // map
 //---------------------------------
+// Marker
+function setMarkerMessage(marker) { 
+  google.maps.event.addListener(marker, 'click', function() {
+    var target = this;
+    var json = $.parseJSON(JSON.stringify(eval("(" + target.getTitle() + ")")));
+
+    // Marker content
+    var content = "<div class='info-content'>"+json.station_name+"</div>";
+    infowindow.setContent(content);
+    infowindow.open(map, target);
+    $('#station_list').val(json.station_name);
+    google.maps.event.addListener(infowindow, 'closeclick', function () {
+        infowindow.close();
+    });    
+  });
+}
+
+var marker_image = new google.maps.MarkerImage("_img/icon.png",
+        null, 
+        // The origin for this image is 0,0.
+        new google.maps.Point(0,0),
+        // The anchor for this image is the base of the flagpole at 0,32.
+        new google.maps.Point(15,0)
+);
+
 function initialize(){
   // Map Style
   var styleArray =[{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"elementType": "labels.icon", "stylers":[{"visibility":"off"}]}];
@@ -69,8 +98,8 @@ function initialize(){
   map.setMapTypeId('map_style');
 
   // Load the station data. When the data comes back, create an overlay.
-  var dataset = []
-  d3.csv("_data/station_data.csv", function(data) { 
+  var dataset = [];
+  d3.json("_data/station_data.Json", function(data) { 
     dataset = data.map(function(d) { return [ d["station_id"], d["station_name"], +d["lat"], +d["long"] ]; }); 
 
     var overlay = new google.maps.OverlayView();
@@ -78,15 +107,6 @@ function initialize(){
     overlay.onAdd = function() {
       var layer = d3.select(this.getPanes().overlayLayer).append("div")
           .attr("class", "stations");
-//MouseTarget
-// // set this as locally scoped var so event does not get confused
-// var me = this;
-
-// // Add a listener - we'll accept clicks anywhere on this div, but you may want
-// // to validate the click i.e. verify it occurred in some portion of your overlay.
-// google.maps.event.addDomListener('.stations', 'click', function() {
-//     google.maps.event.trigger(me);
-// });
 
       // Draw each marker as a separate SVG element.
       // We could use a single SVG, but what size would it have?
@@ -107,16 +127,20 @@ function initialize(){
             .attr("cx", padding)
             .attr("cy", padding);
 
-        // // Add a label.
-        // marker.append("svg:text")
-        //     .attr("x", padding + 7)
-        //     .attr("y", padding)
-        //     .attr("dy", ".31em")
-        //     .text(function(d) { return d.value[0]; });
-
         function transform(d) {
+          var m_title = '{"station_id":"'+d.value[0]+'","station_name":"'+d.value[1]+'"}';
           d = new google.maps.LatLng(d.value[2], d.value[3]);
+          var gmarker = new google.maps.Marker({
+              position: d,
+              map: map,
+              icon: marker_image,
+              title: m_title
+          });         
+          gmarkers.push(gmarker);
+          setMarkerMessage(gmarker);
+
           d = projection.fromLatLngToDivPixel(d);
+
           return d3.select(this)
               .style("left", (d.x - padding) + "px")
               .style("top", (d.y - padding) + "px");
@@ -158,15 +182,15 @@ function initialize(){
 // initial
 function initial_station_list(){
     $.getJSON( "./_data/station_data.json", function( data ) {
-    var items = ["<option value='none'>CHOOSE A STATION</option>"];
-    $.each( data, function( key, val ) {
-      if (val.region == 'San Francisco'){
-        items.push( "<option id='" +'station'+ val.station_id + "'>" + val.station_name + "</option>" );
-      }
+      var items = ["<option value='none'>CHOOSE A STATION</option>"];
+      $.each( data, function( key, val ) {
+        if (val.region == 'San Francisco'){
+          items.push( "<option id='" +'station'+ val.station_id + "'>" + val.station_name + "</option>" );
+        }
+      });
+      $('#station_list').html(items);
     });
-    $('#station_list').html(items);
-  });
-}
+  }
 
 // tab
 $('#region li').click(function(){

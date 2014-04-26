5535a54fc76aa08590b71efce7bf6f49d2892f29
dan tsai <tsai.dan@gmail.com>
2014-04-25 20:24:51 -0700

    sort station list by alpha

--- _js/script.js
+++ _js/script.js
@@ -509,16 +509,18 @@ function drawSingleGraph(graph, stationid) {
 //---------------------------------
 // initial
 function initial_station_list(){
-    $.getJSON( "./_data/station_data.json", function( data ) {
-      var items = ["<option value='none'>CHOOSE A STATION</option>"];
-      $.each( data, function( key, val ) {
-        if (val.region == 'San Francisco'){
-          items.push( "<option id='" +'station'+ val.station_id + "'>" + val.station_name + "</option>" );
-        }
-      });
-      $('#station_list').html(items);
+  $.getJSON( "./_data/station_data.json", function( data ) {
+    var items = ["<option value='none'>CHOOSE A STATION</option>"];
+    data.sort(function(a,b) {return a.station_name.toLowerCase() > b.station_name.toLowerCase() ? 1 : -1;});
+    $.each( data, function( key, val ) {
+      if (val.region == 'San Francisco'){
+        items.push( "<option id='" +'station'+ val.station_id + "'>" + val.station_name + "</option>" );
+      }
     });
-  }
+    $('#station_list').html(items);
+  });
+}
+
 
 // tab
 $('#region li').click(function(){
